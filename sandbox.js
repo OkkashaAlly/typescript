05122d7c797c841a84d4cd71db521960262ca696
OkkashaAlly <steamnature006@gmail.com>
2022-11-19 18:11:04 +0300

    Explicity defining variable types

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,15 @@
-var character = "OkkashaAlly";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) { return console.log(input); });
+// Variables
+var fullName = "Okkasha";
+var age = 22;
+var isMarried = false;
+// Arrays
+var friends = [];
+friends = ["Omar", "Ismail"];
+// Union
+var mixed = ["Okkasha", false, 123];
+var uid;
+uid = 22;
+uid = "abc";
+// Object
+var myObj;
+myObj = { name: "Okkasha", age: 22, isMarried: false };
--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,23 @@
-const character = "OkkashaAlly";
-console.log(character);
+// Variables
+let fullName: string = "Okkasha";
+let age: number = 22;
+let isMarried: boolean = false;
 
-const inputs = document.querySelectorAll("input");
+// Arrays
+let friends: string[] = [];
+friends = ["Omar", "Ismail"];
 
-inputs.forEach(input => console.log(input));
+// Union
+let mixed: (string | number | boolean)[] = ["Okkasha", false, 123];
+let uid: string | number;
+uid = 22;
+uid = "abc";
+
+// Object
+let myObj: {
+  name: string;
+  age: number;
+  isMarried: boolean;
+};
+
+myObj = { name: "Okkasha", age: 22, isMarried: false };
