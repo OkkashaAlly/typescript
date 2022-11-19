71b9ef05c05c8ce841f0b58df31cac86001c6a94
OkkashaAlly <steamnature006@gmail.com>
2022-11-19 20:40:21 +0300

    Type aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,22 +1,6 @@
 "use strict";
-// Variables
-let fullName = "Okkasha";
-let age = 22;
-let isMarried = false;
-// Arrays
-let friends = [];
-friends = ["Omar", "Ismail"];
-// Union
-let mixed = ["Okkasha", false, 123];
-let uid;
-uid = 22;
-uid = "abc";
-// Object
-let myObj;
-myObj = { name: "Okkasha", age: 22, isMarried: false };
-// Dynamic (any)
-let me;
-let mixed2 = [];
-mixed.push("Okkasha");
-mixed.push(2);
-let newObj;
+// let foo: string | number;
+let foo = 10;
+// const greet = (user: { name: string; uid: number | string }) => {};
+const greet = (user) => { };
+greet({ name: "Okkasha", uid: "" });
